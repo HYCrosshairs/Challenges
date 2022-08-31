5cedd7f9125a5052296ff1937fb4180d7e37e855
Hacene YOUNSI <hacene.younsi@yahoo.com>
2022-08-31 23:18:19 +0200

    Impl extract filename from path

--- challenge_2_findFilenameInPath/filenameFinder.h
+++ challenge_2_findFilenameInPath/filenameFinder.h
@@ -1,6 +1,25 @@
 #ifndef _FILENAME_FINDER_H
 #define _FILENAME_FINDER_H
 
-void list_paths_in_folder(const char *folder);
+/**
+ * @brief list files inside a directory recursively
+ * @param folder Path to the directory to list.
+ * @return Show all files and sub directories
+ */
+void list_paths_in_folder(char *folder);
+
+/**
+ * @brief Calculate the length of a string
+ * @param str string to be processed
+ * @return the length of the string
+ */
+unsigned int str_len(char *str);
+
+/**
+ * @brief Extract filename from paths
+ * @param path Path to be processed
+ * @return the filename if exist
+ */
+char* extract_filename_from_path(char* path);
 
 #endif /*_FILENAME_FINDER_H*/
